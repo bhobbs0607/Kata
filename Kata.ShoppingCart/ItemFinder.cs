18d4467cd8cc35f3c849008986f8dd7c5099d91a
bhobbs0607 <bhobbs0607@gmail.com>
2015-10-13 06:21:56 -0400

    Added ItemFinder class

---
+++ Kata.ShoppingCart/ItemFinder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.ShoppingCart
+{
+    public class ItemFinder : IItemFinder
+    {
+        public List<Item> Items { get; set; }
+        
+        public ItemFinder()
+        {
+            Items = new List<Item>();
+        }
+        public Item FindItem(string sku)
+        {
+            return Items.FirstOrDefault(i => i.Name == sku);
+        }
+    }
+}
