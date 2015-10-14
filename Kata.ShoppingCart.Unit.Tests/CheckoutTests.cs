91de05953ca8878d4091b7cfbe78cc97bd53627e
bhobbs0607 <bhobbs0607@gmail.com>
2015-10-13 20:12:41 -0400

    Refactor Unit Test

--- Kata.ShoppingCart.Unit.Tests/CheckoutTests.cs
+++ Kata.ShoppingCart.Unit.Tests/CheckoutTests.cs
@@ -8,25 +8,17 @@ namespace Kata.ShoppingCart.Unit.Tests
     [TestClass]
     public class CheckoutTests
     {
-        
+        Item _itemA = new Item() { Name = "A", Price = 50, Sku = "A" };
+        Item _itemB = new Item() { Name = "B", Price = 30, Sku = "B" };
+        Item _itemC = new Item() { Name = "C", Price = 20, Sku = "C" };
+        Item _itemD = new Item() { Name = "D", Price = 15, Sku = "D" };
+
+        ItemFinder _itemFinder = new ItemFinder();
+        List<SpecialPrice> _specials = new List<SpecialPrice>();
+        Checkout _checkout;
         
         public CheckoutTests()
         {
-            
-
-        }
-
-        [TestMethod]
-        public void CheckoutWithZeroItems()
-        {
-            Item _itemA = new Item() { Name = "A", Price = 50, Sku = "A"};
-            Item _itemB = new Item() { Name = "B", Price = 30, Sku = "B"};
-            Item _itemC = new Item() { Name = "C", Price = 20, Sku = "C"};
-            Item _itemD = new Item() { Name = "D", Price = 15, Sku = "D"};
-
-            ItemFinder _itemFinder = new ItemFinder();
-            List<SpecialPrice> _specials = new List<SpecialPrice>();
-            
             _itemFinder.Items.Add(_itemA);
             _itemFinder.Items.Add(_itemB);
             _itemFinder.Items.Add(_itemC);
@@ -35,8 +27,13 @@ namespace Kata.ShoppingCart.Unit.Tests
             _specials.Add(new SpecialPrice() { ItemName = "A", ItemCount = 3, TotalAmount = 130 });
             _specials.Add(new SpecialPrice() { ItemName = "B", ItemCount = 2, TotalAmount = 45 });
 
-            var _checkout = new Checkout(_itemFinder, _specials);
+            _checkout = new Checkout(_itemFinder, _specials);
 
+        }
+
+        [TestMethod]
+        public void CheckoutWithZeroItems()
+        {
             _checkout.ClearItems();
             Assert.AreEqual(0, _checkout.Total());
         }
@@ -44,23 +41,6 @@ namespace Kata.ShoppingCart.Unit.Tests
         [TestMethod]
         public void CheckoutWithOneItem()
         {
-            Item _itemA = new Item() { Name = "A", Price = 50, Sku = "A" };
-            Item _itemB = new Item() { Name = "B", Price = 30, Sku = "B" };
-            Item _itemC = new Item() { Name = "C", Price = 20, Sku = "C" };
-            Item _itemD = new Item() { Name = "D", Price = 15, Sku = "D" };
-
-            ItemFinder _itemFinder = new ItemFinder();
-            List<SpecialPrice> _specials = new List<SpecialPrice>();
-
-            _itemFinder.Items.Add(_itemA);
-            _itemFinder.Items.Add(_itemB);
-            _itemFinder.Items.Add(_itemC);
-            _itemFinder.Items.Add(_itemD);
-
-            _specials.Add(new SpecialPrice() { ItemName = "A", ItemCount = 3, TotalAmount = 130 });
-            _specials.Add(new SpecialPrice() { ItemName = "B", ItemCount = 2, TotalAmount = 45 });
-
-            var _checkout = new Checkout(_itemFinder, _specials);
 
             _checkout.ClearItems();
             _checkout.Scan("A");
@@ -71,25 +51,8 @@ namespace Kata.ShoppingCart.Unit.Tests
         [TestMethod]
         public void CheckoutWithSpecialItems()
         {
-            Item _itemA = new Item() { Name = "A", Price = 50, Sku = "A" };
-            Item _itemB = new Item() { Name = "B", Price = 30, Sku = "B" };
-            Item _itemC = new Item() { Name = "C", Price = 20, Sku = "C" };
-            Item _itemD = new Item() { Name = "D", Price = 15, Sku = "D" };
+           _checkout.ClearItems();
 
-            ItemFinder _itemFinder = new ItemFinder();
-            List<SpecialPrice> _specials = new List<SpecialPrice>();
-
-            _itemFinder.Items.Add(_itemA);
-            _itemFinder.Items.Add(_itemB);
-            _itemFinder.Items.Add(_itemC);
-            _itemFinder.Items.Add(_itemD);
-
-            _specials.Add(new SpecialPrice() { ItemName = "A", ItemCount = 3, TotalAmount = 130 });
-            _specials.Add(new SpecialPrice() { ItemName = "B", ItemCount = 2, TotalAmount = 45 });
-
-            var _checkout = new Checkout(_itemFinder, _specials);
-
-            _checkout.ClearItems();
             for (int i = 1; i <= 3; i++)
             {
                 _checkout.Scan("A");
@@ -101,24 +64,6 @@ namespace Kata.ShoppingCart.Unit.Tests
         [TestMethod]
         public void CheckoutWithOddNumberOfSpecialItems()
         {
-            Item _itemA = new Item() { Name = "A", Price = 50, Sku = "A" };
-            Item _itemB = new Item() { Name = "B", Price = 30, Sku = "B" };
-            Item _itemC = new Item() { Name = "C", Price = 20, Sku = "C" };
-            Item _itemD = new Item() { Name = "D", Price = 15, Sku = "D" };
-
-            ItemFinder _itemFinder = new ItemFinder();
-            List<SpecialPrice> _specials = new List<SpecialPrice>();
-
-            _itemFinder.Items.Add(_itemA);
-            _itemFinder.Items.Add(_itemB);
-            _itemFinder.Items.Add(_itemC);
-            _itemFinder.Items.Add(_itemD);
-
-            _specials.Add(new SpecialPrice() { ItemName = "A", ItemCount = 3, TotalAmount = 130 });
-            _specials.Add(new SpecialPrice() { ItemName = "B", ItemCount = 2, TotalAmount = 45 });
-
-            var _checkout = new Checkout(_itemFinder, _specials);
-
             _checkout.ClearItems();
             for (int i = 1; i <= 4; i++)
             {
@@ -131,24 +76,6 @@ namespace Kata.ShoppingCart.Unit.Tests
         [TestMethod]
         public void CheckoutWithMultipleItems()
         {
-            Item _itemA = new Item() { Name = "A", Price = 50, Sku = "A" };
-            Item _itemB = new Item() { Name = "B", Price = 30, Sku = "B" };
-            Item _itemC = new Item() { Name = "C", Price = 20, Sku = "C" };
-            Item _itemD = new Item() { Name = "D", Price = 15, Sku = "D" };
-
-            ItemFinder _itemFinder = new ItemFinder();
-            List<SpecialPrice> _specials = new List<SpecialPrice>();
-
-            _itemFinder.Items.Add(_itemA);
-            _itemFinder.Items.Add(_itemB);
-            _itemFinder.Items.Add(_itemC);
-            _itemFinder.Items.Add(_itemD);
-
-            _specials.Add(new SpecialPrice() { ItemName = "A", ItemCount = 3, TotalAmount = 130 });
-            _specials.Add(new SpecialPrice() { ItemName = "B", ItemCount = 2, TotalAmount = 45 });
-
-            var _checkout = new Checkout(_itemFinder, _specials);
-
             _checkout.ClearItems();
             for (int i = 1; i <= 3; i++)
             {
